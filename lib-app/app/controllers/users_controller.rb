6a4cdbd71b4fe46b0c9c32cb040e7402c05cc99f
Chelsea Mangold <narriteration@gmail.com>
2017-04-23 16:02:54 -0700

    sessions new and create working

--- lib-app/app/controllers/users_controller.rb
+++ lib-app/app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  # whitelist permitted form data
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- lib-app/app/models/user.rb
+++ lib-app/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
