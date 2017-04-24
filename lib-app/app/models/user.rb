ebb5a6e62a54141d02432ac2a5f2c139f2cac706
Chelsea Mangold <narriteration@gmail.com>
2017-04-23 17:40:58 -0700

    created LibraryUsers model and associated Users and Libraries

--- lib-app/app/models/user.rb
+++ lib-app/app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+
   has_secure_password
+
+  has_many :library_users, dependent: :destroy
+  has_many :libraries, through: :library_users
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
   end
+  
 end
--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -5,8 +5,14 @@ Rails.application.routes.draw do
   get 'users/new', to: 'users#new', as: 'new_user'
   post 'users', to: 'users#create'
   get 'users/:id', to: 'users#show', as: 'user'
+
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
+
+  get '/libraries', to: 'libraries#index'
+  get '/libraries/new', to: 'libraries#new', as: 'new_library'
+  post '/libraries', to: 'libraries#create'
+  get '/libraries/:id', to: 'libraries#show', as: 'library'
 
 end
